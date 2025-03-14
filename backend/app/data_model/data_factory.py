61447c09b43aa00a04a8ae0b03ab2af98d4b804a
yourisinziri <s7009756@stud.uni-frankfurt.de>
2025-03-14 17:54:43 +0100

    add many feature

--- backend/app/data_model/data_factory.py
+++ backend/app/data_model/data_factory.py
@@ -46,5 +46,4 @@ class DataFactory():
     
     def insert_many_seeds(self, seeds: list) -> bool:
         print(seeds)
-        result = self.mongo.insert_many_items_into_collection('seeds', seeds)
-        return bool(result.inserted_ids)
+        return self.mongo.insert_many_items_into_collection('seeds', seeds)
