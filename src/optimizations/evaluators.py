850e3b3fa113bdc6704ee84e8d3c55ab20269c4f
You-dotcom <Youri.sinziri@baiercompany.com>
2025-05-26 15:45:25 +0200

    Add Coherency Evaluator

--- src/optimizations/evaluators.py
+++ src/optimizations/evaluators.py
@@ -3,14 +3,16 @@ from custom_types import Argument, Noise, Evaluation
 from typing import Generic
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from models.manager import ModelManager
+
 class Evaluator(Generic[Argument] , ABC):
     @abstractmethod
     def evaluate(self, noise: Argument) -> Evaluation:
 
         raise NotImplementedError("Method is not implementet yet")
 
-class MaxDifferenceToMeanBlip2Embedding(Evaluator[Noise]):
+class MaxMeanDivergenceEvaluator(Evaluator[Noise]):
 
     def __init__(self,
                 mean_embds_file_name: str ) -> None:
@@ -28,35 +30,54 @@ class MaxDifferenceToMeanBlip2Embedding(Evaluator[Noise]):
             outputs =self.blip2_model.get_image_features(**inputs)
             blip_embs = outputs.last_hidden_state.mean(dim=1)
 
-        cos = torch.nn.functional.cosine_similarity(noise.image_embs, self.mean_embds)
+        cos = F.cosine_similarity(noise.image_embs, self.mean_embds)
         similarity =cos.item()
         normalised_similarity = (similarity +1)/2
         inversed_similarity = 1 - normalised_similarity
 
         return inversed_similarity
     
-class MaxPromptCoherence(Evaluator[Noise]):
+class MaxPromptCoherenceEvaluator(Evaluator[Noise]):
 
     def __init__(self,
                 mean_embds_file_name: str ) -> None:
         model_manager = ModelManager()
                 
         self.clip_model, self.clip_processor = model_manager.load_clip()
-
-        
-
+    
     def evaluate(self, noise: Noise, prompt: str) -> Evaluation:
         
-        inputs = self.clip_processor(text=[prompt], images=noise.pil, return_tensors="pt", padding=True).to("cuda")
+        inputs = self.clip_processor(
+            text=[prompt],
+            images=noise.pil,
+            return_tensors="pt",
+            padding=True
+        ).to("cuda")
 
         with torch.no_grad():
             outputs = self.clip_model(**inputs)
-            image_emb = outputs.image_embeds  
-            text_emb = outputs.text_embeds    
+            image_embs = outputs.image_embeds
+            text_embs = outputs.text_embeds
+            
+            similarity = F.cosine_similarity(image_embs, text_embs, dim=-1).item()
+            normalised_similarity = (similarity +1)/2
         
-        cos = torch.nn.functional.cosine_similarity(image_emb, text_emb)
-        similarity = cos.item()
-        normalised_similarity = (similarity +1)/2
+        return normalised_similarity
+    
+class MaxQualityEvaluator(Evaluator[Noise]):
+
+    def __init__(self,
+                mean_embds_file_name: str ) -> None:
+        model_manager = ModelManager()
+                
+        self.blip2_model, self.blip_processor = model_manager.load_blip2()
+        
+        self.mean_embds = torch.load(f"mean_embeddings/{mean_embds_file_name}")
         
 
-        return normalised_similarity
+    def evaluate(self, noise: Noise) -> Evaluation:
+        
+        pass
+        
+
+        
--- src/optimizations/genetic_optimization.py
+++ src/optimizations/genetic_optimization.py
@@ -1,13 +1,16 @@
-from custom_types import Device, Noise
+from custom_types import Device, Noise, Argument
+from typing import List, Generic, TypeVar
+from evaluators import Evaluator
 class GeneticOptimizaion():
 
     def __init__ (self
                   generations: int, 
                   population_size: int,
                   selector, 
+                  evaluators: List[Evaluator[Noise]],
                   mutator, 
                   crossover_function,
-                  base_population: List[A], 
+                  base_population: List[Noise], 
                   initial_mutation_rate: float = 0.1,
                   crossover_rate: float = 0.1,
                   elitism_count: int = 0,
@@ -23,6 +26,8 @@ class GeneticOptimizaion():
     def perform_generation(self):
         pass
 
+    def calculate_fitness(self, noise: Noise) -> float:
+        pass
     
    
     
